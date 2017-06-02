102d874b9a38276ba16cfa7991a21570205239dd
Fuse <firebom.sns@gmail.com>
2017-06-02 23:49:10 +0000

    bootstrap design

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,16 +6,22 @@
 　　
 　　@include('commons.error_messages')
     
-    {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
-
+    <div class="row">
+        <div class="col-xs-12, col-sm-offset-8, col-md-offset-8, col-lg-offset-3">
+            {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
+    
 @endsection
